f2839192178a8bba354cc2ebe965048b178762f4
Joao <joaosauer@outlook.com>
2023-12-23 11:15:34 -0300

    create-bigger-question

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\RedirectResponse;
+
+class QuestionController extends Controller
+{
+    public function store(): RedirectResponse
+    {
+
+        $attributes = request()->validate([
+            'question' => ['required', 'min:10'],
+        ]);
+
+        Question::query()->create($attributes);
+
+        return to_route('dashboard');
+    }
+}
